9733d231d2a8f82d89a85dd18a54a14ff3e26952
TWIRINGIYIMANA Obed <obedtwiringiyimana19@gmail.com>
2025-01-21 17:40:18 +0200

    GS kabuga first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+// document.getElementById('learn-more').addEventListener('click', function() {
+//     alert('Thank you for your interest! More information coming soon.');
+// });
+
+
+// const slides = document.querySelector('.hero-slider');
+// const prev = document.querySelector('.prev');
+// const next = document.querySelector('.next');
+// let index = 0;
+
+// function showSlide(i) {
+//     const totalSlides = slides.children.length;
+//     index = (i + totalSlides) % totalSlides;
+//     slides.style.transform = `translateX(-${index * 100}%)`;
+// }
+
+// prev.addEventListener('click', () => showSlide(index - 1));
+// next.addEventListener('click', () => showSlide(index + 1));
+
+
